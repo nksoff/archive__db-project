ddc90d52ca3b990954194f3b8d7b9b31b82e25f5
Nikita <nks.off@gmail.com>
2016-03-08 11:33:00 +0300

    add status api method

--- server/app.py
+++ server/app.py
@@ -1,3 +1,15 @@
+import MySQLdb
 from flask import Flask
 
 app = Flask(__name__)
+app.debug = True
+
+database_config = {
+        'host': 'localhost',
+        'user': 'db',
+        'passwd': 'db',
+        'db': 'db'
+        }
+
+database = MySQLdb.connect(**database_config)
+
--- server/routes.py
+++ server/routes.py
@@ -1,6 +1,20 @@
+# -*- coding: utf-8 -*-
 import MySQLdb
 from app import *
+from flask import jsonify
 
-@app.route('/')
-def hello():
-    return 'hello!'
+@app.route('/db/api/status/', methods=['GET'])
+def status():
+    cursor = database.cursor()
+
+    response = {}
+
+    for entity in ['user', 'thread', 'forum', 'post']:
+        cursor.execute('SELECT COUNT(*) FROM %ss' % entity.capitalize())
+        data = cursor.fetchone()
+        response[entity] = data[0]
+
+    return jsonify({
+            "code": 0,
+            "reponse": response
+            })
