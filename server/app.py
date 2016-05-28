199f66780d4e1c1c5f6265610220403842cf3b98
Nikita <nks.off@gmail.com>
2016-05-29 00:42:35 +0300

    use flask mysql instead of just python one

--- server/app.py
+++ server/app.py
@@ -1,19 +1,16 @@
-import MySQLdb
 from flask import Flask
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'db'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'db'
+app.config['MYSQL_DATABASE_DB'] = 'db'
 
-database_config = {
-        'host': 'localhost',
-        'user': 'db',
-        'passwd': 'db',
-        'db': 'db'
-        }
+mysql = MySQL()
+mysql.init_app(app)
 
 def get_db():
-    database = MySQLdb.connect(**database_config)
-    database.set_character_set('utf8')
-    return database
+    return mysql.get_db()
 
 global_db = get_db()
 def sql_in(values):
